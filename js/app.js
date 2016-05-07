878970c08f75eea5e03f6bfbbbfb43fa7cfc45ed
Sylwia <fellissino@gmail.com>
2016-05-07 02:09:12 +0200

    Poprawienie sekcji dodatkowej

--- js/app.js
+++ js/app.js
@@ -37,21 +37,70 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    var yourChair=document.querySelector(".board2");
+    var yourChair=document.querySelector(".yourChair");
+    var material=document.querySelector(".board2 .material");
+    var transport=document.querySelector(".transport");
     var money=document.querySelector(".board");
     var sum=document.querySelector(".board3");
+    var totalSum=document.querySelector(".totalSum");
 
     var chairInput=document.querySelector("[name*=type]");
-    var chairType=chairInput.value;
+    var materialInput=document.querySelector("[name*=material]");
+    var checkboxInput=document.querySelector(".checkbox");
+
+    var price1=document.querySelector(".price1");
+    var price2=document.querySelector(".price2");
+    var price3=document.querySelector(".price3");
+
+    var chairCost=0;
+    var materialCost=0;
+    var transportCost=0;
+    var totalPrice=chairCost+materialCost+transportCost;
+
 
     chairInput.addEventListener("change", function(event){
-      yourChair.innerHTML="";
+
       if(chairInput.value==1 || chairInput.value==2 || chairInput.value==3){
-        yourChair.innerHTML=chairInput.innerHTML;
+        yourChair.innerHTML=this.options[this.selectedIndex].text;
+        chairCost=parseInt(this.options[this.selectedIndex].getAttribute("price"));
+        price1.innerHTML=chairCost;
+
+        totalPrice=chairCost+materialCost+transportCost;
+        totalSum.innerHTML=totalPrice+"zł";
       }
 
+    });
+    materialInput.addEventListener("change", function(event){
+      if(materialInput.value==1 || materialInput.value==2){
+        material.innerHTML=this.options[this.selectedIndex].text;
+        materialCost=parseInt(this.options[this.selectedIndex].getAttribute("price"));
+        price2.innerHTML=materialCost;
+
+        totalPrice=chairCost+materialCost+transportCost;
+        totalSum.innerHTML=totalPrice+"zł";
+      }
 
     });
+    checkboxInput.addEventListener("change", function(event){
+      if(checkboxInput.checked){
+        transport.innerHTML="Transport";
+        transportCost=parseInt(this.getAttribute("price"));
+        price3.innerHTML=transportCost;
+
+        var totalPrice=chairCost+materialCost+transportCost;
+        totalSum.innerHTML=totalPrice+"zł";
+      }
+      else{
+        transport.innerHTML="";
+        transportCost=0;
+        price3.innerHTML="";
+        totalPrice=chairCost+materialCost+transportCost;
+        totalSum.innerHTML=totalPrice+"zł";
+
+      }
+
+    });
+
 
 
 
